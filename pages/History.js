6a863e492f73ae191c9e0dbbe4fb305ce663c2ae
rivera-fernando <f.rivera@ufl.edu>
2021-03-22 19:15:55 -0400

    Add very basic calendar functionality and some groundwork code to get the user sessions that exist.

--- pages/History.js
+++ pages/History.js
@@ -1,11 +1,64 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Page from "../components/Page";
+import Contain from "../components/Contain";
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import { auth, firestore } from "../firebase";
+import { ContainedButton } from "../components/Buttons";
+
+
+var user = auth.currentUser["uid"];
+const getSessions = async () => {
+  const all_sess = firestore.collection('sessions');
+  const userSessions = await all_sess.where('userID', '==', user).get();
+  if (userSessions.empty) {
+    console.log('No matching documents.');
+    return;
+  }
+  userSessions.forEach(doc => {
+    console.log(doc.id, '=>', doc.data());
+  });
+};
+
+
+
 export default function History() {
   return (
-    <Page>
+    <Contain>
       <Text>History</Text>
-    </Page>
+      <Calendar
+      markedDates={{
+        '2021-03-18': {marked: true, dotColor: 'blue', activeOpacity: 0},
+      }}
+      // Initially visible month. Default = Date()
+      current = {new Date()}
+      // Handler which gets executed on day press. Default = undefined
+      // *********this should open up the existing session if it has one or something like that
+      onDayPress={(day) => {console.log('selected day', day)}}
+      // Handler which gets executed on day long press. Default = undefined
+      onDayLongPress={(day) => {console.log('selected day', day)}}
+      // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+      monthFormat={'yyyy MM'}
+      // Handler which gets executed when visible month changes in calendar. Default = undefined
+      onMonthChange={(month) => {console.log('month changed', month)}}
+      // Do not show days of other months in month page. Default = false
+      hideExtraDays={true}
+      // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+      firstDay={1}
+      // Hide day names. Default = false
+      hideDayNames={true}
+      // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+      onPressArrowLeft={subtractMonth => subtractMonth()}
+      // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+      onPressArrowRight={addMonth => addMonth()}
+      // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+      disableAllTouchEventsForDisabledDays={true}
+      // Replace default month and year title with custom one. the function receive a date as parameter.
+      renderHeader={(date) => {/*Return JSX*/}}
+      // Enable the option to swipe between months. Default = false
+      enableSwipeMonths={true}
+      />
+      <ContainedButton handlePress={getSessions} text="GetSessions" />
+    </Contain>
   );
 }
 
