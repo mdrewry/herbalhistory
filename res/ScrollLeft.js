4d5fd573f08c59b5083cf674f81ed5469d052ac3
Rohan <rohan.samanta@hotmail.com>
2021-03-23 00:23:46 -0400

    added fields and scroll indicators, removed icons

---
+++ res/ScrollLeft.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+export default function ScrollLeft() {
+    return(
+        <Svg 
+            width="7" 
+            height="9" 
+            viewBox="0 0 7 9" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg">
+        <Path 
+            d="M7 7.935L2.67341 4.5L7 1.0575L5.66802 0L1.42953e-07 4.5L5.66802 9L7 7.935Z" 
+            fill="#183A1D"
+        />
+        </Svg>
+
+
+    );
+}
